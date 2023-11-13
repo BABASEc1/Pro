10f0ef74b4f0ac800a60ec6ed85308e705b268fb
B A B A <136027010+BABASEc1@users.noreply.github.com>
2023-11-13 13:05:49 +0500

    Update Run.py

--- Run.py
+++ Run.py
@@ -1,15 +1,1 @@
-import os, platform
-try:
-    import httpx
-except:
-    os.system('pip install httpx')
-os.system('git pull')
-print('\033[1;32m [•] Follow For Updates')
-os.system('xdg-open https://www.facebook.com/hackerbaba4')
-import httpx
-bit = platform.architecture()[0]
-if bit == '64bit':
-    from Pro import Baba
-    Baba()
-elif bit == '32bit':
-    print("\x1b[1;91mOpps Sorry Brother Your Mobile Not Support This Tools")
+print ('wait for next update working some executions and scrape list thanks for your pateints')
